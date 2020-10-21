a7d3a8ab1c1b523e5bdfe4ff1a32f8e09c182b28
shrey27tri01 <shrey.tripathi@iiitb.org>
2020-10-21 15:46:39 +0530

    2nd try for static files

--- project4/wsgi.py
+++ project4/wsgi.py
@@ -12,7 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project4.settings')
-from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
