d328d8e02b7ca9d260949390088eaf4b6420dc67
shrey27tri01 <shrey.tripathi@iiitb.org>
2020-10-21 15:06:00 +0530

    Added allowed host

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'just-post-it.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
